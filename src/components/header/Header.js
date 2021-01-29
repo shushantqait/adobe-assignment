85f506e9d46be07d6fb339e489ca4ff9f372989f
shushantqait <shushantarora@qainfotech.com>
2021-01-30 02:48:13 +0530

    Added Search in Header File

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -7,11 +7,11 @@ import {
     Flex,
     Image,
     Button,
-    Divider
+    Divider,
+    SearchField
 } from "@adobe/react-spectrum";
 import TabsContainer from '../tabsContainer';
 import websiteLogo from '../../assets/logo.svg';
-import Search from '@spectrum-icons/workflow/Search';
 
 const tabs = [
     { id: 1, name: 'Creative Cloud' },
@@ -23,15 +23,19 @@ function Header() {
     return (
         <div className="Header">
             <Navbar bg="white" expand="lg" className="pb-1 pt-0">
-                <Navbar.Brand href="#" className="d-flex col-2 pr-0 m-0 ml-1">
+                <Navbar.Brand href="#" className="d-flex col-2 col-sm-10 col-md-4 col-lg-3 col-xl-2 pr-0 m-0 ml-1 justify-content-center">
                     <Image width="size-300" alignSelf="auto" marginEnd="size-75" bottom="size-10" position="relative" justifySelf="auto" src={websiteLogo} alt="Adobe Logo" UNSAFE_className="d-flex vertical-align-middle" />
                     <Text marginStart="size-100" UNSAFE_className="Header__icon-label d-flex vertical-align-middle font-weight-bold">Adobe Exchange</Text>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav" className="col-lg-10 col-md-12 col-sm-12 pl-0 ml-0 pr-0 d-flex Header__navbar-flex-wrap justify-content-between">
+                <Navbar.Collapse id="basic-navbar-nav" className="col-lg-10 col-md-12 col-sm-12 pl-0 ml-0 pr-0 Header__navbar-flex-wrap justify-content-between">
                     <TabsContainer tabs={tabs} />
-                    <Flex direction="row" wrap justifyContent="space-between" width="15%" UNSAFE_className="Header__responsive-box">
-                        <Search aria-label="Search" UNSAFE_className="cursor-pointer" />
+                    <Flex direction="row" wrap justifyContent="space-between" width="30%" UNSAFE_className="Header__responsive-box">
+                        <SearchField
+                            isQuiet
+                            placeholder="Search App"
+                            UNSAFE_className="search-border-none"
+                        />
                         <Button marginStart="size-400" variant="cta">
                             Sign In
                         </Button>
